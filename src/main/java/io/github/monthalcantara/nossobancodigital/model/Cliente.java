cae25d8bd53a7d766429a88045304cfc6c8c9fcb
montival Junior <montival_junior@yahoo.com.br>
2020-10-01 14:18:54 -0300

    Adicionada entidade Cliente

---
+++ src/main/java/io/github/monthalcantara/nossobancodigital/model/Cliente.java
@@ -0,0 +1,44 @@
+package io.github.monthalcantara.nossobancodigital.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = " O nome do Cliente é obrigatório")
+    @Column(length = 20, unique = true, nullable = false)
+    private String nome;
+
+    @NotEmpty(message = " O Sobrenome do Cliente é obrigatório")
+    @Column(length = 100)
+    private String sobrenome;
+
+    @CPF(message = " CPF inválido")
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String cnh;
+
+    private LocalDate DataDeNascimento;
+
+}
