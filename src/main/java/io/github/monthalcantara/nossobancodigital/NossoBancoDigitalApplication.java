c5826528364a8f017dff901222f0bbf6fd36e544
montival Junior <montival_junior@yahoo.com.br>
2020-10-01 12:30:23 -0300

    Criacao do projeto

---
+++ src/main/java/io/github/monthalcantara/nossobancodigital/NossoBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.nossobancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/monthalcantara/nossobancodigital/NossoBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.nossobancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
