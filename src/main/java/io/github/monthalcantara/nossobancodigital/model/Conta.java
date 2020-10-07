768001fbb5ba83b60da66c943d5704fef5eccccc
monthalcantara <montival_junior@yahoo.com.br>
2020-10-06 21:36:18 -0300

    adicionada classe conta

---
+++ src/main/java/io/github/monthalcantara/nossobancodigital/model/Conta.java
@@ -0,0 +1,26 @@
+package io.github.monthalcantara.nossobancodigital.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Conta {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 4)
+    private String agencia;
+
+    @Column(length = 8)
+    private String conta;
+
+    private String CodigoBanco="191";
+
+    @OneToOne
+
+    private Cliente cliente;
+}
