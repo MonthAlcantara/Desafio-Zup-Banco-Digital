815ebc92ed585c45dfb5971a1fae559e6a1a589a
monthalcantara <montival_junior@yahoo.com.br>
2020-10-11 13:16:04 -0300

    Criada anotação para verificação de maior idade

---
+++ src/main/java/io/github/monthalcantara/nossobancodigital/validation/annotations/MaiorIdade.java
@@ -0,0 +1,21 @@
+package io.github.monthalcantara.nossobancodigital.validation.annotations;
+
+import io.github.monthalcantara.nossobancodigital.validation.validators.MaiorIdadeValidator;
+import io.github.monthalcantara.nossobancodigital.validation.validators.UnicoCNHValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MaiorIdadeValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MaiorIdade {
+
+    String message() default "{io.github.monthalcantara.nossobancodigital.validation.annotations.MaiorIdade}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
---
+++ src/main/java/io/github/monthalcantara/nossobancodigital/validation/validators/MaiorIdadeValidator.java
@@ -0,0 +1,16 @@
+package io.github.monthalcantara.nossobancodigital.validation.validators;
+
+import io.github.monthalcantara.nossobancodigital.validation.annotations.MaiorIdade;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class MaiorIdadeValidator implements ConstraintValidator<MaiorIdade, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
+           LocalDate agora = LocalDate.now();
+        return Period.between(localDate, agora).toTotalMonths() >= 216;
+    }
+}
