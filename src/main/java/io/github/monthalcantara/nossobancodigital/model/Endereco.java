fdcbe96a7a2140404fb544bec8cf0caa6bfe4ca8
monthalcantara <montival_junior@yahoo.com.br>
2020-11-04 22:56:09 -0300

    Adicionado metodo verificar se campos estao preenchidos

--- src/main/java/io/github/monthalcantara/nossobancodigital/model/Endereco.java
+++ src/main/java/io/github/monthalcantara/nossobancodigital/model/Endereco.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -32,4 +33,28 @@ public class Endereco {
 
     private String estado;
 
+    public boolean verificaTodosCamposEstaoCompletos() {
+        return this.cep != null && !(this.cep.isEmpty())
+                && this.rua != null && !(this.rua.isEmpty())
+                && this.bairro != null && !(this.bairro.isEmpty())
+                && this.complemento != null && !(this.complemento.isEmpty())
+                && this.cidade != null && !(this.cidade.isEmpty())
+                && this.estado != null && !(this.estado.isEmpty());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(cep, endereco.cep) &&
+                Objects.equals(bairro, endereco.bairro) &&
+                Objects.equals(cidade, endereco.cidade) &&
+                Objects.equals(estado, endereco.estado);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cep, bairro, cidade, estado);
+    }
 }
